fb312ef94af646c74ce03e59d217594192bf2441
rithasyed <161039057+Ritha24@users.noreply.github.com>
2024-11-25 15:12:17 +0530

    changes

--- api/v1/__init__.py
+++
@@ -1,5 +0,0 @@
-from api.v1.endpoints import router as endpoints_router
-from api.v1.symbols_endpoints import router as symbols_router
-
-
-__all__ = ["endpoints_router","symbols_router"]
--- utils/symbols.py
+++
@@ -1,5 +0,0 @@
-
-def get_symbols():
-    with open('symbols.txt') as f:
-        symbols = [line.strip() for line in f]
-    return symbols
